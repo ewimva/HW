837e04373525b09633e88758247cf06fe62d5fbf
ewimva <etookebaeva@gmail.com>
2025-04-10 23:05:45 +0600

    HW_2

--- src/main.py
+++ src/main.py
@@ -2,19 +2,29 @@ import flet as ft
 
 
 def main(page: ft.Page):
+    # установка заголовка
+    page.title = "Привет мир"
 
-    def change_name(e):
-        print(name_input.value)
+    friends = ['Elmira', 'Nasiba', 'Nika', 'Nursultan', 'Nurlis', 'Emir']
 
-    
+    def name(check):
+        if name_input.value in friends:
+            print('Drug naiden', name_input.value)
+        else:
+            print('Drug ne naiden', name_input.value)
+
+    # функция, которая будет вызываться при изменении значения текстового поля
+    #def change_name(e):
+    #    print(name_input.value)
+
+    # создание текстового поля
     name_input = ft.TextField(
-        label="Some text",
-        on_click=change_name
+        label="введите имя",  # текст подсказки
+        on_change=name,  # функция, которая будет вызываться при изменении значения
     )
 
-
+    # добавление текстового поля на страницу(окно)
     page.add(name_input)
-
 
 
 ft.app(main)
